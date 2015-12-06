2ba9390123eb41f4eda5e04ff2be6ffa8a01965d
Egor Suvorov <egor.suvorov@gmail.com>
2015-12-06 16:35:19 +0300

    Add CollectionsTest

---
+++ src/test/java/ru/spbau/mit/CollectionsTest.java
@@ -0,0 +1,144 @@
+package ru.spbau.mit;
+
+import org.junit.Test;
+import ru.spbau.mit.testclasses.Funcs;
+import ru.spbau.mit.testclasses.Val;
+import ru.spbau.mit.testclasses.ValA;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class CollectionsTest {
+    @Test
+    public void testMap() {
+        List<ValA> source = Arrays.asList(new ValA(1), new ValA(2), new ValA(3));
+        List<Val> expected = Arrays.asList(new Val(1), new Val(4), new Val(9));
+
+        Iterable<Val> res = Collections.map(new Function1<Val, Val>() {
+            @Override
+            public Val apply(Val arg) {
+                return new Val(arg.val * arg.val);
+            }
+        }, source);
+        for (int step = 0; step < 2; step++) {
+            List<Val> got = new LinkedList<>();
+            for (Val x : res) {
+                got.add(x);
+            }
+            assertEquals(expected, got);
+        }
+    }
+
+    @Test
+    public void testFilter() {
+        List<Integer> source = Arrays.asList(1, 2, 1, null, 1, 4);
+        List<Integer> expected = Arrays.asList(1, 2, 1, 1, 4);
+
+        Iterable<Integer> res = Collections.filter(new Predicate<Number>() {
+            @Override
+            public Boolean apply(Number arg) {
+                return arg != null;
+            }
+        }, source);
+        for (int step = 0; step < 2; step++) {
+            List<Integer> got = new LinkedList<>();
+            for (Integer x : res) {
+                got.add(x);
+            }
+            assertEquals(expected, got);
+        }
+    }
+
+    @Test
+    public void testTakeWhile() {
+        List<Integer> source = Arrays.asList(1, 2, 1, null, 1, 4);
+        List<Integer> expected = Arrays.asList(1, 2, 1);
+
+        Iterable<Integer> res = Collections.takeWhile(new Predicate<Number>() {
+            @Override
+            public Boolean apply(Number arg) {
+                return arg != null;
+            }
+        }, source);
+        for (int step = 0; step < 2; step++) {
+            List<Integer> got = new LinkedList<>();
+            for (Integer x : res) {
+                got.add(x);
+            }
+            assertEquals(expected, got);
+        }
+    }
+
+    @Test
+    public void testTakeUnless() {
+        List<Integer> source = Arrays.asList(1, 2, 1, null, 1, 4);
+        List<Integer> expected = Arrays.asList(1, 2, 1);
+
+        Iterable<Integer> res = Collections.takeUnless(new Predicate<Number>() {
+            @Override
+            public Boolean apply(Number arg) {
+                return arg == null;
+            }
+        }, source);
+        for (int step = 0; step < 2; step++) {
+            List<Integer> got = new LinkedList<>();
+            for (Integer x : res) {
+                got.add(x);
+            }
+            assertEquals(expected, got);
+        }
+    }
+
+    @Test
+    public void testFoldr() {
+        List<Integer> source = Arrays.asList(1, 2, 3);
+        LinkedList<String> initial = new LinkedList<>();
+        initial.add("init");
+
+        LinkedList<String> result = Collections.foldr(new Function2<Object, List<String>, LinkedList<String>>() {
+            @Override
+            public LinkedList<String> apply(Object arg1, List<String> arg2) {
+                LinkedList<String> result = new LinkedList<String>();
+                result.add(String.format("%s+(%s)", arg1.toString(), arg2.get(0)));
+                for (String s : arg2) {
+                    result.add(s);
+                }
+                return result;
+            }
+        }, source, initial);
+
+        assertArrayEquals(new String[]{
+                "1+(2+(3+(init)))",
+                "2+(3+(init))",
+                "3+(init)",
+                "init"
+        }, result.toArray());
+    }
+
+    @Test
+    public void testFoldl() {
+        List<Integer> source = Arrays.asList(1, 2, 3);
+        LinkedList<String> initial = new LinkedList<>();
+        initial.add("init");
+
+        LinkedList<String> result = Collections.foldl(new Function2<List<String>, Object, LinkedList<String>>() {
+            @Override
+            public LinkedList<String> apply(List<String> arg1, Object arg2) {
+                LinkedList<String> result = new LinkedList<String>();
+                result.add(String.format("(%s)+%s", arg1.get(0), arg2.toString()));
+                for (String s : arg1) {
+                    result.add(s);
+                }
+                return result;
+            }
+        }, initial, source);
+
+        assertArrayEquals(new String[]{
+                "(((init)+1)+2)+3",
+                "((init)+1)+2",
+                "(init)+1",
+                "init"
+        }, result.toArray());
+    }
+}
